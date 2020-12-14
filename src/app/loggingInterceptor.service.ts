c4e5e81b2b381a8d5deef75a1b65af1a2507c09e
Purushottam K.C <Kseapuru20@gmail.com>
2020-12-14 12:28:29 +0545

    changed some logging codes

--- src/app/loggingInterceptor.service.ts
+++ src/app/loggingInterceptor.service.ts
@@ -2,19 +2,20 @@ import { HttpEventType, HttpHandler, HttpInterceptor, HttpRequest } from '@angul
 import { tap } from 'rxjs/operators'
 export class loggingInterceptorservice implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler) {
-        console.log('Ongoing request');
+        console.log('Outgoing request');
+        console.log(req.url);
         return next.handle(req).pipe(
             tap(ev => {
                 if(ev.type === HttpEventType.Response){
-                    console.log('response with full body receive');
+                    console.log('Incoming response');
                     console.log(ev.body);
                     
                 }
-                else if (ev.type === HttpEventType.Sent){
-                    console.log(' request sent' );
-                    console.log(ev.type);
+                // else if (ev.type === HttpEventType.Sent){
+                //     console.log(' request sent' );
+                //     console.log(ev.type);
                     
-                }
+                // }
             })
         );
     }
